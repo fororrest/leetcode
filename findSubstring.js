23cbce2ebfb44d2a7874a90f6ceeb8aa608c6182
Xun Li <xun.li@lazada.com>
2018-11-26 09:41:28 +0800

    feat: 30. Substring with Concatenation of All Words

--- findSubstring.js
+++ findSubstring.js
@@ -1,8 +1,178 @@
+/* eslint-disable */
+/**
+ * 30. Substring with Concatenation of All Words
+ * You are given a string, s, and a list of words, words,
+ * that are all of the same length. Find all starting indices of
+ * substring(s) in s that is a concatenation of each word in words
+ * exactly once and without any intervening characters.
+ * https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/description/
+ */
+
+/**
+ * getAllIndex
+ * @param {string} s
+ * @param {string} word
+ */
+function getAllIndex(s, word) {
+  const indexes = [];
+
+  // const sLen = s.length;
+  const firstIdx = s.indexOf(word);
+
+  if (firstIdx === -1) {
+    return [];
+  }
+  indexes.push(firstIdx);
+
+  let idx = firstIdx;
+  while (idx !== -1) {
+    idx = s.indexOf(word, idx + 1);
+    if (idx !== -1) {
+      indexes.push(idx);
+    }
+  }
+
+  return indexes;
+}
+
+function serialArray(arr) {
+  const lengthArr = [];
+  const productArr = [];
+  const result = [];
+  let length = 1;
+  for (var i = 0; i < arr.length; i++) {
+    const len = arr[i].length;
+    lengthArr.push(len);
+    const product = i === 0 ? 1 : arr[i - 1].length * productArr[i - 1];
+    productArr.push(product);
+    length *= len;
+  }
+  for (var i = 0; i < length; i++) {
+    let resultItem = [];
+    for (let j = 0; j < arr.length; j++) {
+      resultItem.push(arr[j][Math.floor(i / productArr[j]) % lengthArr[j]]);
+    }
+    result.push(resultItem);
+  }
+  return result;
+}
+
+/**
+ * 全排列
+ * @param {*} xs
+ */
+function perm(xs) {
+  let ret = [];
+
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if(!rest.length) {
+      ret.push([xs[i]])
+    } else {
+      for(let j = 0; j < rest.length; j = j + 1) {
+        ret.push([xs[i]].concat(rest[j]))
+      }
+    }
+  }
+  return ret;
+}
+
 /**
  * @param {string} s
  * @param {string[]} words
  * @return {number[]}
  */
-var findSubstring = function(s, words) {
+function findSubstring(s, words) {
+  let result = [];
 
-};
+  console.time('idxArray')
+  const idxArray = words.map((item) => {
+    const indexes = getAllIndex(s, item);
+    return indexes.map(idx => ({
+      word: item,
+      index: idx,
+    }));
+  });
+
+  console.timeEnd('idxArray')
+  // console.log(idxArray)
+  console.log(idxArray.map(a => a.map(item => item.index)));
+  console.log('----------');
+
+  console.time('pArray')
+  let pArray = [];
+  if (idxArray.length === 0) {
+    return [];
+  } else if (idxArray.length === 1) {
+    idxArray[0].forEach(item => {
+      if (!result.includes(item.index)) {
+        result.push(item.index);
+      }
+    });
+    return result;
+  }
+
+  // pArray = idxArray.reduce((a, b) => a.reduce((r, v) => r.concat(b.map(w => [].concat(v, w))), []));
+  pArray = serialArray(idxArray);
+
+  console.timeEnd('pArray')
+
+  // console.log(pArray)
+  // console.log(pArray.map(a => a.map(item => item.index)));
+  // console.log('----------');
+
+  console.time('fullList')
+  const fullList = [];
+  pArray.forEach(item => {
+    perm(item).forEach(arr => {
+      fullList.push(arr);
+    })
+  });
+  console.timeEnd('fullList')
+
+  console.log(fullList.length);
+  // console.log(fullList.map(a => a.map(item => item.index)));
+  // console.log('----------');
+
+  console.time('filterList')
+  const filterList = fullList.filter((itemArr) => {
+    const result = itemArr.reduce((a, b) => {
+      if (a && b.index > a.index && b.index === a.index + a.word.length) {
+        // console.log(`a: ${a.index}, b: ${b.index}`);
+        return b;
+      }
+      return null;
+    });
+    return Boolean(result);
+  });
+
+  console.timeEnd('filterList')
+
+  // console.log(filterList);
+  // console.log('----------')
+
+  filterList.forEach(item => {
+    if (!result.includes(item[0].index)) {
+      result.push(item[0].index);
+    }
+  });
+
+  console.timeEnd('共花费了')
+  return result;
+}
+
+// findSubstring('', []);
+// findSubstring('a', ['a'])
+// findSubstring('barfoothefoobarman', ['foo', 'bar']);
+// findSubstring('barfoofoobarthefoobarman', ["bar","foo","the"]);
+// findSubstring('wordgoodstudentgoodword', ['word', 'student']);
+// findSubstring('wordgoodgoodgoodbestword', ['word', 'good', 'best', 'word']);
+// findSubstring('wordgoodgoodgoodbestword', ["word","good","best","good"]);
+// findSubstring('mississippi', ["is"]);
+// findSubstring("abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa", ["cac","aaa","aba","aab","abc"]);
+const result = findSubstring("abcbaaaccaabbcababaaabccaabccccbbccbaabcbccacacacabcbbbacbcbbccabaccbbbcbaabbabbaaaacaacbcacbbaacbcbcbabbbcacbbacaacbbbcacccbbcacabbbacaccbcbaababa", ["bcb","baa","cac","aca","cca"]);
+console.log(result);
+
+
+module.exports = findSubstring;
--- findSubstring.test.js
+++ findSubstring.test.js
@@ -1,5 +1,61 @@
 const findSubstring = require('./findSubstring.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(findSubstring("barfoothefoobarman", ["foo","bar"])).toBe([0, 9]);
-});
+const testCase = [
+  ["a", ["a"], [0]],
+  ["", [], []],
+  ["mississippi", ["is"], [1, 4]],
+  ["barfoothefoobarman", ["foo", "bar"], [0, 9]],
+  ["barfoofoobarthefoobarman", ["bar","foo","the"], [6, 9, 12]],
+  ["wordgoodstudentgoodword", ["word", "student"], []],
+  ["wordgoodgoodgoodbestword", ["word", "good", "best", "word"], []],
+  ["wordgoodgoodgoodbestword", ["word","good","best","good"], [8]],
+  ["abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa", ["cac","aaa","aba","aab","abc"], [97]],
+  ["abcbaaaccaabbcababaaabccaabccccbbccbaabcbccacacacabcbbbacbcbbccabaccbbbcbaabbabbaaaacaacbcacbbaacbcbcbabbbcacbbacaacbbbcacccbbcacabbbacaccbcbaababa", ["bcb","baa","cac","aca","cca"], [35]]
+];
+
+testCase.forEach(item => {
+  test(`${item[0]} [${item[1]}] should equal [${item[2]}]`, () => {
+    const result = findSubstring(item[0], item[1]);
+    expect(result).toEqual(item[2]);
+  });
+})
+
+// test('"a" ["a"] should equal [0] ', () => {
+//   const result = findSubstring('a', ['a']);
+//   expect(result).toEqual([0]);
+// });
+
+// test('"" [] should equal [] ', () => {
+//   const result = findSubstring('', []);
+//   expect(result).toEqual([]);
+// });
+
+// test('"mississippi" ["is"] should equal [1, 4] ', () => {
+//   const result = findSubstring('mississippi', ["is"]);
+//   expect(result).toEqual([1, 4]);
+// });
+
+// test('"barfoothefoobarman" ["foo", "bar"] should equal [0, 9] ', () => {
+//   const result = findSubstring('barfoothefoobarman', ["foo", "bar"]);
+//   expect(result).toEqual([0, 9]);
+// });
+
+// test('"barfoofoobarthefoobarman" ["bar","foo","the"] should equal [6, 9, 12] ', () => {
+//   const result = findSubstring('barfoofoobarthefoobarman', ["bar","foo","the"]);
+//   expect(result).toEqual([6, 9, 12]);
+// });
+
+// test('"wordgoodstudentgoodword" ["word", "student"] should equal [] ', () => {
+//   const result = findSubstring('wordgoodstudentgoodword', ["word", "student"]);
+//   expect(result).toEqual([]);
+// });
+
+// test('"wordgoodgoodgoodbestword" ["word", "good", "best", "word"] should equal [] ', () => {
+//   const result = findSubstring('wordgoodgoodgoodbestword', ['word', 'good', 'best', 'word']);
+//   expect(result).toEqual([]);
+// });
+
+// test('"wordgoodgoodgoodbestword" ["word","good","best","good"] should equal [] ', () => {
+//   const result = findSubstring('wordgoodgoodgoodbestword', ["word","good","best","good"]);
+//   expect(result).toEqual([8]);
+// });
